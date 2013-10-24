05b727648afab64581ff3c59c5b24375025d4015
Shohei Aoki <shoaok@gmail.com>
2013-10-24 20:44:29 +0900

    modified

--- public/javascripts/agent.js
+++ public/javascripts/agent.js
@@ -1,7 +1,5 @@
 $(function(){
-  var userAgent = window.navigator.userAgent.toLowerCase();
-  alert(userAgent);
-  if(userAgent.indexOf("iPod") != -1 || userAgent.indexOf("iPad") != -1 || userAgent.indexOf("iPhone") > != -1 || userAgent.indexOf("Android") != -1 ){
+  if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || (navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0)) {
     $("#main").load("mobile.html")
     } else {
     $("#main").load("desktop.html")
