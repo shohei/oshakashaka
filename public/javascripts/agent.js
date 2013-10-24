534297386a9c969562a9e66d7d83e1c72c4a8ab9
Shohei Aoki <shoaok@gmail.com>
2013-10-24 20:41:38 +0900

    modified logic

--- public/javascripts/agent.js
+++ public/javascripts/agent.js
@@ -1,7 +1,7 @@
 $(function(){
   var userAgent = window.navigator.userAgent.toLowerCase();
   alert(userAgent);
-  if(userAgent.indexOf("iPod") > 0 || userAgent.indexOf("iPad") > 0 || userAgent.indexOf("iPhone") > 0 || userAgent.indexOf("Android") > 0){
+  if(userAgent.indexOf("iPod") != -1 || userAgent.indexOf("iPad") != -1 || userAgent.indexOf("iPhone") > != -1 || userAgent.indexOf("Android") != -1 ){
     $("#main").load("mobile.html")
     } else {
     $("#main").load("desktop.html")
