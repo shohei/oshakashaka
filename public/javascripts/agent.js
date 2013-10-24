7f72e175311db252e5225c2492f681f5135a46ad
Shohei Aoki <shoaok@gmail.com>
2013-10-24 20:39:03 +0900

    modified user agent filtering

--- public/javascripts/agent.js
+++ public/javascripts/agent.js
@@ -1,7 +1,7 @@
 $(function(){
   var userAgent = window.navigator.userAgent.toLowerCase();
   alert(userAgent);
-  if(userAgent == "iPod" || userAgent == "iPad" || userAgent == "iPhone" || userAgent == "Android"){
+  if(userAgent.indexOf("iPod") > 0 || userAgent.indexOf("iPad") > 0 || userAgent.indexOf("iPhone") > 0 || userAgent.indexOf("Android") > 0){
     $("#main").load("mobile.html")
     } else {
     $("#main").load("desktop.html")
