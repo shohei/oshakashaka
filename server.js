7fefd58622289b1ccadafba3e0e4fc35c612bc57
Shohei Aoki <shoaok@gmail.com>
2013-10-25 21:37:24 +0900

    copied app.js to server.js

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ var app = module.exports = express.createServer();
 var io = io.listen(app);
 
 //Serial port
-// var serialport = require('serialport');
-// var portName = '/dev/tty.usbmodemfa131';
-// var sp = new serialport.SerialPort(portName,{
-// 	baudRate: 9600,
-// 	dataBits: 8,
-// 	parity: 'none',
-// 	stopBits: 1,
-// 	flowControl: false,
-// 	parser: serialport.parsers.readline("\n")
-// });
+//var serialport = require('serialport');
+//var portName = '/dev/tty.usbmodemfa131';
+//var sp = new serialport.SerialPort(portName,{
+//	baudRate: 9600,
+//	dataBits: 8,
+//	parity: 'none',
+//	stopBits: 1,
+//	flowControl: false,
+//	parser: serialport.parsers.readline("\n")
+//});
 
 // Configuration
 
@@ -41,28 +41,45 @@ app.configure('production', function(){
 });
 
 // Routes
-app.get('/', routes.index);
+app.get('/',routes.index);
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
+
+var usersound = {};
 //socket.io code
 io.sockets.on('connection', function (socket) {
 	socket.emit('news_event',{event_code : 'websocket is ready for '+socket.id});
 
+	var soundId = Math.floor(Math.random()*3)+1;
+	usersound[socket.id] = soundId;
+	console.log(usersound);
+
+	socket.emit("image_event",usersound[socket.id]);
+	socket.broadcast.emit("desktop_image_event",usersound[socket.id]);
+
 	socket.on('x_snare',function(data){
-		socket.broadcast.emit('x_snare',{devid:socket.id});
+		socket.broadcast.emit('x_snare',usersound[socket.id]);
 	});
 
 	socket.on('gyro_val',function(data){
 		socket.broadcast.emit('gyro_val',{x_val : data["x_val"], y_val : data["y_val"],z_val : data["z_val"]});
 	});
 
-	// data from serial port
-	// sp.on('data', function(input){
-	// 	var arduino_data = input;
-	// 	socket.emit('pin_state',{state : arduino_data});
-	// });
-
+	socket.on('disconnect', function () {
+	 //  var flag = 1;
+	 //  for(var sokid in usersound)
+		// {
+	 //      if(usersound[sokid] == usersound[socket.id])
+	 //   	   {
+	 //   	   	flag = 0;
+  // 		    }
+		// }
+		// if(flag == 1){
+	 //		socket.broadcast.emit("desktop_refresh",usersound[socket.id]);
+ 	 // 	}
+		socket.broadcast.emit("desktop_refresh",usersound[socket.id]);
+	 	//delete usersound[socket.id];
+	 	console.log(usersound);
+	});
 });
-
-
