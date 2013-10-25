42a9b9f4c87c99d73e555f5146bc76e7bccc1349
Shohei Aoki <shoaok@gmail.com>
2013-10-26 05:59:38 +0900

    merged

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express')
   , routes = require('./routes');
 var io = require('socket.io');
 var app = module.exports = express.createServer();
-var io = io.listen(app);
+var io = io.listen(app, {'log level': 2});
 
 //Serial port
 //var serialport = require('serialport');
@@ -47,19 +47,65 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 
 
 var usersound = {};
+var usercount = {};
+var currentMode = {};
+// var lastValue = {};
+// var lastlastValue = {};
 //socket.io code
 io.sockets.on('connection', function (socket) {
 	socket.emit('news_event',{event_code : 'websocket is ready for '+socket.id});
 
-	var soundId = Math.floor(Math.random()*3)+1;
+	var soundId = Math.floor(Math.random()*5)+1;
 	usersound[socket.id] = soundId;
+	usercount[socket.id] = 0;
+	currentMode[socket.id] = "x";
+	// lastValue[socket.id] = 0;
+	// lastlastValue[socket.id] = 0;
 	console.log(usersound);
 
 	socket.emit("image_event",usersound[socket.id]);
+	socket.emit("switch_to_x");
 	socket.broadcast.emit("desktop_image_event",usersound[socket.id]);
 
 	socket.on('x_snare',function(data){
-		socket.broadcast.emit('x_snare',usersound[socket.id]);
+		// lastlastValue[socket.id] = lastValue[socket.id];
+		// lastValue[socket.id] = data;
+		usercount[socket.id] =  parseInt(usercount[socket.id]) + 1;
+		if(usercount[socket.id] % 20 == 0){
+			socket.broadcast.emit("effect",{"soundId":soundId,"currentMode":currentMode[socket.id]});
+		}
+		if(usercount[socket.id] ==  40){
+			var cmode = currentMode[socket.id];
+			console.log(cmode);
+			switch(cmode){
+			  case "x":
+   			    currentMode[socket.id] = "y";
+			    socket.emit("switch_to_y");
+			    console.log("switch to y");
+			    socket.broadcast.emit("desktop_jack_shake",soundId);
+			    //socket.broadcast.emit("desktop_ending",soundId);
+			    usercount[socket.id] = 0;
+			    break;
+			  case "y":
+   			    currentMode[socket.id] = "z";
+			    socket.emit("switch_to_z");
+			    console.log("switch to z");
+			    socket.broadcast.emit("desktop_jack_up",soundId);
+			    usercount[socket.id] = 0;
+			    break;
+   			  case "z":
+   			    currentMode[socket.id] = "x";
+			    socket.emit("switch_to_x");
+			    console.log("switch to x");
+			    socket.broadcast.emit("desktop_ending",soundId);
+			    usercount[socket.id] = 0;
+    		    break;
+			}
+		}
+
+		// socket.broadcast.emit('x_snare',{"soundId" : usersound[socket.id], "usercount" : usercount[socket.id],"lastValue":lastValue[socket.id],"lastlastValue":lastlastValue[socket.id]});
+		socket.broadcast.emit('x_snare',{"soundId" : usersound[socket.id], "usercount" : usercount[socket.id]});
+		console.log(usercount[socket.id]);
 	});
 
 	socket.on('gyro_val',function(data){
